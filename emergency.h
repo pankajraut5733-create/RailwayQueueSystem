caec75b2755fb8023fa48614f499d95ace752d80
Bhaskar Mhanta <bhaskarmhanta3@gmail.com>
2026-05-24 21:21:55 +0530

    Improved emergency module declarations

--- emergency.h
+++ emergency.h
@@ -2,15 +2,15 @@
 #define EMERGENCY_H
 
 #include "common.h"
-
+/* Stack Initialization */
 void initStack(struct Stack *s);
 
 int isStackEmpty(struct Stack *s);
 
 int isStackFull(struct Stack *s);
-
+/* Add Emergency Passenger */
 int push(struct Stack *s, struct Passenger p);
-
+/* Serve Emergency Passenger */
 struct Passenger pop(struct Stack *s);
 
 void displayStack(struct Stack *s);
