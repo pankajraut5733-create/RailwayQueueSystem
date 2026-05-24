1f7bf04133f26f25ac4d4367ca378ec51d380609
Vedant Dhange <vedantdhange777@gmail.com>
2026-05-24 21:34:31 +0530

    Improved waiting list header documentation

--- waiting_list.h
+++ waiting_list.h
@@ -2,11 +2,11 @@
 #define WAITING_LIST_H
 
 #include "common.h"
-
+/* Add Passenger to Waiting List */
 void addToWaitingList(struct Passenger p);
-
+/* Remove Passenger from Waiting List */
 struct Passenger removeFromWaitingList();
-
+/* Display Waiting List */
 void displayWaitingList();
 
 #endif
