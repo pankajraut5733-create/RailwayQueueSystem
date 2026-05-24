02264ede6bf5eb9f3a59fb9a59178f8d3d739a82
pankajraut5733-create <rautp4863@gmail.com>
2026-05-24 22:38:44 +0530

    Improved train management header file

--- train.h
+++ train.h
@@ -2,11 +2,11 @@
 #define TRAIN_H
 
 #include "common.h"
-
+/* Initialize Train Records */
 void initTrains();
-
+/* Search Train by ID */
 int findTrain(int trainId);
-
+/* Display All Trains */
 void displayTrains();
 
 #endif
